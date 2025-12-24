d81cb8f9c2dee4cf3ff9994e2737a530baa8ddb3
Tam97123 <219667310+Tam97123@users.noreply.github.com>
2025-12-24 02:28:52 +0000

    Auto for tricky store is enough, remove AI because that damn fateh7 is using it

--- Module/Yuri/select_app_neccesary.sh
+++ Module/Yuri/select_app_neccesary.sh
@@ -25,11 +25,9 @@ com.google.android.ims!
 com.google.android.safetycore!
 com.google.android.apps.walletnfcrel!
 com.google.android.apps.nbu.paisa.user!
-gr.nikolasspyr.integritycheck!
 com.youhu.laifu!
 com.whatsapp!
 com.whatsapp.w4b!
-com.openai.chatgpt!
 com.reveny.nativecheck!
 icu.nullptr.nativetest!
 com.android.nativetest!
